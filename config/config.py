8f9d686c68df7c48fe4d524a3a0aef82082b180f
Sergei Kolomiets <s_kolomiyets@mail.ru>
2021-02-16 22:11:35 +0300

    add config

---
+++ config/config.py
@@ -0,0 +1,47 @@
+import yaml
+import typing as tp
+
+
+class DotDict(dict):
+    """
+    Dict-like object, that allows to access values using dot notation:
+        dct = DotDict({"one": 1, "two": 2})
+        assert dct.one == 1
+    """
+
+    __getattr__ = dict.__getitem__
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+    def __init__(self, data: tp.Dict[tp.Any, tp.Any]) -> None:
+        """
+        :param tp.Dict[tp.Any, tp.Any] data: data to populate self
+        :returns: None
+        """
+        for key, value in data.items():
+            if isinstance(value, dict):
+                value = DotDict(value)
+            elif isinstance(value, list):
+                value = [
+                    DotDict(item) if isinstance(item, dict) else item for item in value
+                ]
+
+            self[key] = value
+
+
+class Config(DotDict):
+    """
+    DotDict child dedicated to be used to store MarioNet config contents
+    """
+
+    @classmethod
+    def from_file(cls, filepath: str = "config.yaml"):
+        """
+        Reads yaml file and populates self with its contents
+        :param str filepath: path to config file
+        :returns: populated Config instance
+        """
+        with open(filepath, "r") as config_file:
+            config_dict = yaml.load(config_file)
+        print(filepath)
+        return cls(config_dict)
--- extract_landmarks.py
+++ extract_landmarks.py
@@ -1,17 +1,30 @@
 import os
+import re
 from glob import glob
 from tqdm import tqdm
 
 from preprocessor import LandmarkExtractor
 from skimage import io
 
-if __name__ == '__main__':
-    path_to_images = glob('/home/serk0/Business/new_live/VoxCeleb1/Faces/*/*/*/*')
-    landmark_extractor = LandmarkExtractor(landmark_type='3D', device='cpu')
+from config.config import Config
+
+
+def extract_landmarks(config):
+
+    path_to_images = glob(os.path.join(config.path_to_voxceleb, config.subfolders))
+
+    landmark_extractor = LandmarkExtractor(
+        landmark_type=config.landmark_type, device=config.device
+    )
 
     for path_to_image in tqdm(path_to_images):
-        saving_path = path_to_image[:39] + '/Landmarks' + path_to_image[45:]
-        saving_folder = '/'.join(saving_path.split('/')[:-1])
+        saving_path = re.sub("/Faces", "/Landmarks", path_to_image)
+        saving_folder = "/".join(saving_path.split("/")[:-1])
         os.makedirs(saving_folder, exist_ok=True)
         image_with_landmarks = landmark_extractor(path_to_image)
         io.imsave(saving_path, image_with_landmarks)
+
+
+if __name__ == "__main__":
+    config = Config.from_file("config/config.yaml")
+    extract_landmarks(config)
