c59f1fa2eb07ba118ac52c4077d12c597abe2e1c
Sergei Kolomiets <s_kolomiyets@mail.ru>
2021-02-17 17:36:36 +0300

    Added Dataloader

---
+++ dataloader.py
@@ -0,0 +1,130 @@
+import os
+import numpy as np
+import re
+
+import torch
+from glob import glob
+from PIL import Image
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from skimage import io
+
+np.random.seed(1)
+
+
+class MarioNetDataset(Dataset):
+    def __init__(
+        self,
+        folder: str,
+        faces_structure: str,
+        identity_structure: str,
+        video_structue: str,
+        n_target_image=4,
+        resized_image_height=128,
+        resized_image_width=128,
+    ):
+        self.folder = folder
+        self.faces = faces_structure
+        self.identity_structure = os.listdir(os.path.join(folder, identity_structure))
+        self.video_structue = video_structue
+        self.n_target_image = n_target_image
+        self.resized_image_height = resized_image_height
+        self.resized_image_width = resized_image_width
+
+    def __len__(self):
+        return len(self.identity_structure)
+
+    def __getitem__(self, index):
+        source_identity = self.identity_structure[index]
+        target_identity = np.random.choice(self.identity_structure, size=1)[0]
+        target_identity_subfolders = os.listdir(
+            os.path.join(self.folder, self.video_structue.format(target_identity))
+        )
+
+        path_to_source_faces = glob(
+            os.path.join(self.folder, self.faces.format(source_identity, "*"))
+        )
+
+        target_identity_subfolder_name = np.random.choice(
+            target_identity_subfolders, size=1
+        )[0]
+
+        path_to_target_faces = glob(
+            os.path.join(
+                self.folder,
+                self.faces.format(target_identity, target_identity_subfolder_name),
+            ),
+        )
+
+        source_face_path = np.random.choice(path_to_source_faces, size=1)[0]
+        target_face_path = np.random.choice(
+            path_to_target_faces, size=self.n_target_image
+        )
+
+        result = {
+            "source_image": None,
+            "source_landmarks": None,
+            "target_images": None,
+            "target_landmarks": None,
+        }
+
+        source_image = io.imread(source_face_path)
+        source_landmarks = io.imread(re.sub("Faces", "Landmarks", source_face_path))
+
+        target_images = []
+        target_landmarks = []
+
+        for target_face in target_face_path:
+            target_image = io.imread(target_face)
+            target_image_landmarks = io.imread(
+                re.sub("Faces", "Landmarks", target_face)
+            )
+
+            target_images.append(self.__resize_image(torch.tensor(target_image.T)))
+            target_landmarks.append(
+                self.__resize_image(torch.tensor(target_image_landmarks.T))
+            )
+
+        result["source_image"] = self.__resize_image(torch.tensor(source_image.T))
+        result["source_landmarks"] = self.__resize_image(
+            torch.tensor(source_landmarks.T)
+        )
+        result["target_images"] = torch.stack(target_images)
+        result["target_landmarks"] = torch.stack(target_landmarks)
+
+        return result
+
+    def __resize_image(self, image):
+        return transforms.Resize(
+            (self.resized_image_height, self.resized_image_width),
+            interpolation=Image.NEAREST,
+        ).forward(image)
+
+
+if __name__ == "__main__":
+    folder = "/home/serk0/Business/new_live/VoxCeleb1/"
+    faces_structure = "Faces/{0}/1.6/{1}/*"
+    identity_structure = "Faces/"
+    video_structue = "Faces/{0}/1.6/"
+    n_target_image = 4
+    resized_image_height = 128
+    resized_image_width = 128
+
+    marionet_dataset = MarioNetDataset(
+        folder,
+        faces_structure,
+        identity_structure,
+        video_structue,
+        n_target_image,
+        resized_image_height,
+        resized_image_width,
+    )
+
+    # print(marionet_dataset[3]["target_images"].size())
+
+    dl = DataLoader(marionet_dataset, batch_size=4)
+
+    for b in dl:
+        break
+
+    print(b["target_images"].size())
