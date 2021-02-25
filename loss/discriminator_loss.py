2032f79799215a7ee2e71cd3fbb40466d18e54d8
Vadim Titov <titov.vn@phystech.edu>
2021-02-25 19:45:03 +0300

    hinge loss implemented

--- loss/discriminator_loss.py
+++ loss/discriminator_loss.py
@@ -1,9 +1,29 @@
-
 import torch
 import torch.nn as nn
 
 
+class HingeDiscriminatorLoss(nn.Module):
+    """
+    HingeLoss functor for Discriminator
+    """
+    def __init__(self):
+        super(HingeDiscriminatorLoss, self).__init__()
+
+    def __call__(self, real_discriminator_features,
+                       fake_discriminator_features):
+
+        ones_tensor = torch.ones_like(real_discriminator_features)
+
+        real_part = torch.max(torch.tensor([0, torch.mean(ones_tensor - real_discriminator_features)]))
+        fake_part = torch.max(torch.tensor([0, torch.mean(ones_tensor - fake_discriminator_features)]))
+
+        return real_part + fake_part
+
+
 class DiscriminatorLossPatches(nn.Module):
+    """
+    BCELoss functor (default) for Discriminator
+    """
     def __init__(self, base_loss=nn.BCELoss):
         super(DiscriminatorLossPatches, self).__init__()
         self.base_loss = base_loss()
@@ -15,3 +35,6 @@ class DiscriminatorLossPatches(nn.Module):
             return self.base_loss(features_tensor, torch.zeros(features_tensor.size(), device=features_tensor.device))
         else:
             raise ValueError("Incorrect target_type: {}".format(target_type))
+
+
+
