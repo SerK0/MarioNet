42718a28c7309aedf5fe0075813a106caa11d9ca
Sergei Kolomiets <s_kolomiyets@mail.ru>
2021-02-26 18:45:14 +0300

    correction

--- model/dataloader.py
+++ model/dataloader.py
@@ -5,13 +5,9 @@ import re
 import torch
 from glob import glob
 from PIL import Image
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 from torchvision import transforms
 from skimage import io
-
-from .config import Config
-
-np.random.seed(1)
 
 
 class MarioNetDataset(Dataset):
@@ -20,14 +16,14 @@ class MarioNetDataset(Dataset):
         folder: str,
         faces_structure: str,
         identity_structure: str,
-        video_structue: str,
+        video_structure: str,
         n_target_image=4,
         image_size=128,
     ):
         self.folder = folder
         self.faces = faces_structure
         self.identity_structure = os.listdir(os.path.join(folder, identity_structure))
-        self.video_structue = video_structue
+        self.video_structure = video_structure
         self.n_target_image = n_target_image
         self.image_size = image_size
 
@@ -45,7 +41,7 @@ class MarioNetDataset(Dataset):
         source_identity = self.identity_structure[index]
         target_identity = np.random.choice(self.identity_structure, size=1)[0]
         target_identity_subfolders = os.listdir(
-            os.path.join(self.folder, self.video_structue.format(target_identity))
+            os.path.join(self.folder, self.video_structure.format(target_identity))
         )
 
         path_to_source_faces = glob(
@@ -95,36 +91,3 @@ class MarioNetDataset(Dataset):
     def __resize_image(self, image):
         image = torch.tensor(image)
         return self.transforms.forward(image)
-
-
-if __name__ == "__main__":
-    import sys
-
-    print(sys.path)
-    from pathlib import Path
-
-    cfg = Config.from_file(Path(__file__).parent / "config/config.yaml")
-
-    marionet_dataset = MarioNetDataset(
-        cfg.dataloader.folder,
-        cfg.dataloader.faces_structure,
-        cfg.dataloader.identity_structure,
-        cfg.dataloader.video_structue,
-        cfg.dataloader.n_target_image,
-        cfg.dataloader.image_size,
-    )
-
-    dl = DataLoader(marionet_dataset, batch_size=4)
-
-    for b in dl:
-        break
-
-    print(b["target_images"].size())
-
-    for i, image in enumerate(b["target_images"][0]):
-        print(image.size())
-        io.imsave("/home/serk0/i{}.jpg".format(i), image.detach().numpy().T)
-
-    for i, image in enumerate(b["target_landmarks"][0]):
-        print(image.size())
-        io.imsave("/home/serk0/l{}.jpg".format(i), image.detach().numpy().T)
--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -1,1 +1,27 @@
-print("xxxx")
+from pathlib import Path
+
+cfg = Config.from_file(Path(__file__).parent / "config/config.yaml")
+
+marionet_dataset = MarioNetDataset(
+    cfg.dataloader.folder,
+    cfg.dataloader.faces_structure,
+    cfg.dataloader.identity_structure,
+    cfg.dataloader.video_structure,
+    cfg.dataloader.n_target_image,
+    cfg.dataloader.image_size,
+)
+
+dl = DataLoader(marionet_dataset, batch_size=4)
+
+for b in dl:
+    break
+
+print(b["target_images"].size())
+
+# for i, image in enumerate(b["target_images"][0]):
+#     print(image.size())
+#     io.imsave("/home/serk0/i{}.jpg".format(i), image.detach().numpy().T)
+
+# for i, image in enumerate(b["target_landmarks"][0]):
+#     print(image.size())
+#     io.imsave("/home/serk0/l{}.jpg".format(i), image.detach().numpy().T)
