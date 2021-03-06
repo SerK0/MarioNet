3d97b983fc816f55df20543881b9129de1c49ed4
Vadim Titov <titov.vn@phystech.edu>
2021-03-06 18:35:32 +0300

    spectral norm scaling

--- marionet/model/marionet.py
+++ marionet/model/marionet.py
@@ -79,3 +79,7 @@ class MarioNet(nn.Module):
 
         zxy = self.blender(zx, zy)
         return self.decoder(zxy, s)
+
+    def spectral_norm_weight(self):
+        for layer in self.children():
+            layer.fill(torch.nn.utils.spectral_norm(layer))
