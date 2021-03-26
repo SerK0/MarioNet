ad8158d57492049d4f8533998208f25bd071033e
Sergei Kolomiets <s_kolomiyets@mail.ru>
2021-03-26 22:23:44 +0300

    remove replace=true, wandb save only samples

--- utils.py
+++ utils.py
@@ -131,7 +131,6 @@ class Trainer:
                     {
                         "images/epoch_{}".format(epoch): [
                             wandb.Image(input_images, caption="Input Images"),
-                            wandb.Image(reenacted_image, caption="Generated Image"),
                         ]
                     }
                 )
@@ -290,7 +289,7 @@ class Trainer:
                 driver_landmarks=batch["driver_landmarks"],
             ).detach()
 
-            samples.append(denorm(reenacted_images[0]))
+            samples.append(reenacted_images[0]))
 
             generator_results = torch.cat(samples, dim=2)
 
